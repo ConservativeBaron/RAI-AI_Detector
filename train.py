fa66053ed80f25adb349f3327809e329f07c8df3
ConservativeBaron <keratin@courvix.com>
2023-04-27 09:13:53 -0500

    🐱‍👤 Conform to pylint standards

--- train.py
+++ train.py
@@ -3,8 +3,8 @@ Training module for AI text detection
 
 Author: Keratin
 """
+# pylint: disable=import-error
 import pandas as pd
-import tensorflow as tf
 from tensorflow.keras.layers import Input, Embedding, Conv1D, MaxPooling1D, Flatten, Dense
 from tensorflow.keras.models import Model
 from tensorflow.keras.preprocessing.text import Tokenizer
@@ -14,54 +14,101 @@ from sklearn.model_selection import train_test_split
 
 def load_data():
     """Load dataset from CSV file and split into training and testing sets."""
-    df = pd.read_csv('./datasets/labeled_data.csv')
-    train_text, test_text, train_labels, test_labels = train_test_split(df['message'], df['class'], test_size=0.2)
-    return train_text, test_text, train_labels, test_labels
+
+    dataf = pd.read_csv(
+        './datasets/labeled_data.csv'
+    )
+    trains_text, tests_text, trains_labels, tests_labels = train_test_split(
+        dataf['message'],
+        dataf['class'],
+        test_size=0.2
+    )
+
+    return trains_text, tests_text, trains_labels, tests_labels
 
 
-def tokenize_data(train_text, test_text):
+def tokenize_data(trained_text, tested_text):
     """Tokenize text data using Tokenizer from Keras and pad sequences to a fixed length."""
-    tokenizer = Tokenizer(num_words=5000)
-    tokenizer.fit_on_texts(train_text)
-    train_sequences = pad_sequences(tokenizer.texts_to_sequences(train_text), maxlen=100)
-    test_sequences = pad_sequences(tokenizer.texts_to_sequences(test_text), maxlen=100)
-    return tokenizer, train_sequences, test_sequences
+
+    tokenizer_data = Tokenizer(
+        num_words=5000
+    )
+    tokenizer_data.fit_on_texts(
+        trained_text
+    )
+    trained_sequences = pad_sequences(
+        tokenizer_data.texts_to_sequences(trained_text),
+        maxlen=100
+    )
+    tested_sequences = pad_sequences(
+        tokenizer_data.texts_to_sequences(tested_text),
+        maxlen=100
+    )
+
+    return tokenizer_data, trained_sequences, tested_sequences
 
 
 def define_model():
     """Define a convolutional neural network model using Keras."""
-    input_layer = Input(shape=(100,))
-    embedding_layer = Embedding(input_dim=5000, output_dim=50)(input_layer)
-    conv_layer = Conv1D(filters=128, kernel_size=5, activation='relu')(embedding_layer)
+
+    input_layer = Input(
+        shape=(100,)
+    )
+    embedding_layer = Embedding(
+        input_dim=5000,
+        output_dim=50
+    )(input_layer)
+    conv_layer = Conv1D(
+        filters=128,
+        kernel_size=5,
+        activation='relu'
+    )(embedding_layer)
     pooling_layer = MaxPooling1D(pool_size=5)(conv_layer)
     flatten_layer = Flatten()(pooling_layer)
-    output_layer = Dense(units=1, activation='sigmoid')(flatten_layer)
-    model = Model(inputs=input_layer, outputs=output_layer)
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    return model
+    output_layer = Dense(
+        units=1,
+        activation='sigmoid'
+    )(flatten_layer)
+    model_defined = Model(
+        inputs=input_layer,
+        outputs=output_layer
+    )
+    model_defined.compile(
+        loss='binary_crossentropy',
+        optimizer='adam',
+        metrics=['accuracy']
+    )
+
+    return model_defined
 
 
-def train_model(model, train_sequences, train_labels, test_sequences, test_labels):
+def train_model(model_training,sequences_training,labels_training,sequences_testing,labels_testing):
     """Train the convolutional neural network model."""
-    model.fit(
-        train_sequences, 
-        train_labels, 
-        epochs=1, 
+
+    model_training.fit(
+        sequences_training,
+        labels_training,
+        epochs=1,
         batch_size=1, # Works better, dont be a puss
-        validation_data=(test_sequences, test_labels)
+        validation_data=(sequences_testing, labels_testing)
     )
 
 
-def save_model(model):
+def save_model(saved_model):
     """Save the trained convolutional neural network model to a file."""
-    model.save('Models/ai_detection_model.h5')
+
+    saved_model.save(
+        'Models/ai_detection_model.h5'
+    )
 
 
-def classify_input(model, tokenizer):
+def classify_input(model_classify, tokenizer_classify):
     """Classify a text string input as AI or human using the trained model and tokenizer."""
 
     while True:
-        user_input = input('Enter a text string to classify (or "exit" to quit): ')
+        user_input = input(
+            'Enter a text string to classify (or "exit" to quit): '
+        )
 
         if user_input.lower() == 'exit':
             exit()
@@ -69,18 +116,34 @@ def classify_input(model, tokenizer):
         if user_input.lower() == '':
             continue
 
-        sequence = pad_sequences(tokenizer.texts_to_sequences([user_input]), maxlen=100)
-        prediction = model.predict(sequence)[0][0]
+        sequence = pad_sequences(
+            tokenizer_classify.texts_to_sequences([user_input]), 
+            maxlen=100
+        )
+        prediction = model_classify.predict(sequence)[0][0]
 
         if prediction > 0.5:
-            print(f"Message classified as AI.")
+            print("Message classified as AI.")
         else:
-            print(f"Message is classified as human.")
+            print("Message is classified as human.")
 
 if __name__ == '__main__':
     train_text, test_text, train_labels, test_labels = load_data()
-    tokenizer, train_sequences, test_sequences = tokenize_data(train_text, test_text)
+    tokenizer, train_sequences, test_sequences = tokenize_data(
+        train_text,
+        test_text
+    )
     model = define_model()
-    train_model(model, train_sequences, train_labels, test_sequences, test_labels)
-    save_model(model)
-    classify_input(model, tokenizer)
+    train_model(model,
+                train_sequences,
+                train_labels,
+                test_sequences,
+                test_labels
+    )
+    save_model(
+        model
+    )
+    classify_input(
+        model,
+        tokenizer
+    )
